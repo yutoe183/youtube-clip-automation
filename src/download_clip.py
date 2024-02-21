cee890b67eacc2ec353eeb131d167d4eac802651
Y.M <yutoe183@gmail.com>
2024-02-22 05:30:26 +0900

    元動画削除オプション追加、DLエラー無視

--- src/download_clip.py
+++ src/download_clip.py
@@ -1,3 +1,4 @@
+import sys # argv
 import os # remove, path.isfile
 import csv # reader
 import yt_dlp # YoutubeDL
@@ -33,9 +34,12 @@ def getResults(path): # ファイルから結果を取得
         results.append((subStrBegin(row[0], "=", "&"), row[4], timeToSecond(row[2]), timeToSecond(row[3]), row[0][row[0].find("http"):row[0].find("&")]))
   return results
 
-def downloadClip(results): # 各動画のダウンロードと切り抜き
-  REMOVE_ORIGINAL = False # ダウンロードした元動画を削除するか
-  OPTION = {"outtmpl": "%(upload_date)s[%(id)s].%(ext)s"}
+def downloadClip(results, remove_original): # 各動画のダウンロードと切り抜き
+  OPTION = {
+    "outtmpl": "%(upload_date)s[%(id)s].%(ext)s", # 出力形式 投稿日[動画ID].mp4
+    "ignoreerrors": True, # エラーを無視して続行
+  }
+  MAX_RETRY_DOWNLOAD = 6 # ダウンロードに失敗した際の最大再試行回数
   len_results = len(results)
   with yt_dlp.YoutubeDL(OPTION) as ydl:
     count_same_id = 0
@@ -45,25 +49,31 @@ def downloadClip(results): # 各動画のダウンロードと切り抜き
       path_download = filename + ".mp4"
       path_clip = filename + "_clip" + str(count_same_id) + ".mp4"
       if not os.path.isfile(path_clip): # 出力先ファイルが既に存在する場合は動画生成しない
-        if not os.path.isfile(path_download): # 存在しない場合のみダウンロード
+        retry_download = MAX_RETRY_DOWNLOAD
+        while not os.path.isfile(path_download) and retry_download > 0: # 存在しない場合のみダウンロード
+          retry_download -= 1
           ydl.download([url])
-        videoclip = VideoFileClip(path_download).subclip(sec_begin, sec_end)
-        videoclip.write_videofile(path_clip, codec="mpeg4", bitrate="1000000000")
+        if os.path.isfile(path_download): # ダウンロードに失敗した場合はスキップ
+          if sec_begin < sec_end: # 時刻指定に誤りがある場合はスキップ
+            videoclip = VideoFileClip(path_download).subclip(sec_begin, sec_end)
+            videoclip.write_videofile(path_clip, codec="mpeg4", bitrate="1000000000")
       count_same_id += 1
       id_next = ""
       if i < len_results - 1:
         id_next = results[i + 1][0]
       if (id != id_next):
         count_same_id = 0
-        if REMOVE_ORIGINAL: # 用済みになった動画は、オプションがTrueなら削除
-          os.remove(path_download)
+        if remove_original: # 用済みになった動画は、オプションがTrueなら削除
+          if os.path.isfile(path_download):
+            os.remove(path_download)
 
-def execute(path):
+def execute(path, remove_original):
   results = getResults(path)
-  downloadClip(results)
+  downloadClip(results, remove_original)
 
 def main():
-  execute("../extract/results.txt")
+  remove_original = len(sys.argv) > 1 and (sys.argv[1] == "-d" or sys.argv[1] == "-D") # ダウンロードした元動画を削除するか
+  execute("../extract/results.txt", remove_original)
 
 if __name__ == "__main__":
   main()
--- src/video.sh
+++ src/video.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-# 作業用ルートディレクトリで実行 source src/video.sh arg1 [arg2]
-# 引数(必須1個 + 任意1個): 作成したディレクトリ名, 検索文字列
+# 作業用ルートディレクトリで実行 source src/video.sh arg1 [arg2, arg3]
+# 引数(必須1個 + 任意2個): 作成したディレクトリ名, 検索文字列, -d (ダウンロードした元動画を削除する場合)
 
 source venv/yt-dlp_moviepy/bin/activate
 cd data/"$1"/extract
@@ -13,7 +13,12 @@ fi
 cd ..
 mkdir clip
 cd clip
-python ../../../src/download_clip.py
+if [ $# -ge 3 ]
+then
+  python ../../../src/download_clip.py "$3"
+else
+  python ../../../src/download_clip.py
+fi
 cd ..
 mkdir dst
 cd dst
