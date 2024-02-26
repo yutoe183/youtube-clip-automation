fd69198d4449a9ccfcf349de687d1fa4cf4cfecf
Y.M <yutoe183@gmail.com>
2024-02-27 02:05:44 +0900

    チャットダウンロードのスキ ップ機能追加

--- src/chat.sh
+++ src/chat.sh
@@ -1,23 +1,29 @@
 #!/bin/sh
-# 作業用ルートディレクトリで実行 source src/chat.sh arg1 arg2 arg3 [arg4, arg5]
+# 作業用ルートディレクトリで実行 source src/chat.sh arg1 arg2 [arg3, arg4]
 # 引数(必須3個 + 任意2個): 作成するディレクトリ名, YouTubeのChannel ID, 検索文字列, 取得開始日(YYYYMMDD), 取得終了日(YYYYMMDD)
 
 source venv/yt-dlp_moviepy/bin/activate
-mkdir -p data/"$1"/live_chat
-cd data/"$1"/live_chat
-if [ $# -ge 5 ]
+if [ ! -d data/"$1"/live_chat ]
 then
-  yt-dlp --skip-download --write-subs --write-comments --dateafter $4 --datebefore $5 -o "%(upload_date)s[%(id)s]" "https://www.youtube.com/channel/$2"
-elif [ $# == 4 ]
-then
-  yt-dlp --skip-download --write-subs --write-comments --dateafter $4 -o "%(upload_date)s[%(id)s]" "https://www.youtube.com/channel/$2"
-else
-  yt-dlp --skip-download --write-subs --write-comments -o "%(upload_date)s[%(id)s]" "https://www.youtube.com/channel/$2"
+  mkdir -p data/"$1"/live_chat
+  cd data/"$1"/live_chat
+  if [ $# -ge 5 ]
+  then
+    yt-dlp --skip-download --write-subs --write-comments --dateafter $4 --datebefore $5 -o "%(upload_date)s[%(id)s]" "https://www.youtube.com/channel/$2"
+  elif [ $# == 4 ]
+  then
+    yt-dlp --skip-download --write-subs --write-comments --dateafter $4 -o "%(upload_date)s[%(id)s]" "https://www.youtube.com/channel/$2"
+  else
+    yt-dlp --skip-download --write-subs --write-comments -o "%(upload_date)s[%(id)s]" "https://www.youtube.com/channel/$2"
+  fi
+  cd ../../..
 fi
-cd ..
-mkdir extract
-cd extract
-grep -E "$3" ../live_chat/* > search.txt
-python ../../../src/clustering_chat.py "$3"
-cd ../../..
+if [ $# -ge 3 ]
+then
+  mkdir -p data/"$1"/extract
+  cd data/"$1"/extract
+  grep -E "$3" ../live_chat/* > search.txt
+  python ../../../src/clustering_chat.py "$3"
+  cd ../../..
+fi
 deactivate
